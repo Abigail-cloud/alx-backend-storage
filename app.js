5fe0ec578ed7c55c055e520302b0d79e7cf97bcd
ToluwalaseAO <toluwalaseabigail@outlook.com>
2024-11-02 00:48:31 +0100

    no sql

---
+++ app.js
@@ -0,0 +1,19 @@
+//Conditioning 
+
+/**
+ * strict equality : === to check for the datatype
+ * equality : == e.g 1 == "1"
+ */
+// if(5+2 === "7"){
+//  console.log("Seun is a techpreneur")
+// }
+if ("not" == "good") {
+ console.log(false)
+} else {
+ console.log(true)
+}
+//  look around create at least 3 conditions from your environment
+// use the if condition ---- 3
+// ifelse  ---3
+
+// dom
